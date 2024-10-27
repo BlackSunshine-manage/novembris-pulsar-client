bc62a90c4f2dcee5c444eedb6043ae7497e307bc
Edik <snd009@mail.ru>
2024-10-27 03:11:49 +0300

    create a decoration methods  PulsarClient

--- src/main/java/novembris/pulsar/client/NovLoggablePulsarClient.java
+++ src/main/java/novembris/pulsar/client/NovLoggablePulsarClient.java
@@ -1,8 +1,10 @@
 package novembris.pulsar.client;
 
-import org.apache.pulsar.client.api.PulsarClient;
-import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.*;
+import org.apache.pulsar.client.api.transaction.TransactionBuilder;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 
 public final class NovLoggablePulsarClient extends NovPulsarClient {
@@ -19,4 +21,76 @@ public final class NovLoggablePulsarClient extends NovPulsarClient {
             throw new RuntimeException(e);
         }
     }
+
+    ProducerBuilder<byte[]> newProducer() {
+        return client.newProducer();
+    };
+
+    <T> ProducerBuilder<T> newProducer(Schema<T> schema) {
+        return client.newProducer(schema);
+    };
+
+    ConsumerBuilder<byte[]> newConsumer() {
+        return newConsumer();
+    };
+
+    <T> ConsumerBuilder<T> newConsumer(Schema<T> schema) {
+        return client.newConsumer(schema);
+    };
+
+    ReaderBuilder<byte[]> newReader() {
+        return client.newReader();
+    };
+
+    <T> ReaderBuilder<T> newReader(Schema<T> schema) {
+        return client.newReader(schema);
+    };
+
+    /** @deprecated */
+    @Deprecated
+    <T> TableViewBuilder<T> newTableViewBuilder(Schema<T> schema) {
+        return client.newTableViewBuilder(schema);
+    };
+
+    TableViewBuilder<byte[]> newTableView() {
+        return client.newTableView();
+    };
+
+    <T> TableViewBuilder<T> newTableView(Schema<T> schema) {
+        return client.newTableView(schema);
+    };
+
+    void updateServiceUrl(String serviceUrl) throws PulsarClientException {
+        client.updateServiceUrl(serviceUrl);
+    };
+
+    /** @deprecated */
+    @Deprecated
+    CompletableFuture<List<String>> getPartitionsForTopic(String topic) {
+        return client.getPartitionsForTopic(topic, true);
+    }
+
+    CompletableFuture<List<String>> getPartitionsForTopic(String topic, boolean metadataAutoCreationEnabled) {
+        return client.getPartitionsForTopic(topic, metadataAutoCreationEnabled);
+    };
+
+    void close() throws PulsarClientException {
+        client.close();
+    };
+
+    CompletableFuture<Void> closeAsync() {
+        return client.closeAsync();
+    };
+
+    void shutdown() throws PulsarClientException {
+        client.shutdown();
+    };
+
+    boolean isClosed() {
+        return client.isClosed();
+    };
+
+    TransactionBuilder newTransaction() {
+        return client.newTransaction();
+    };
 }
